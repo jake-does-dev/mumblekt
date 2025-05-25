b207d1c142a0872368b71d8d41c6ff8b5bc19147
Jake Allan <jjemail1995@gmail.com>
2025-05-25 17:40:50 +0100

    feat(proto): add test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,5 @@
 package dev.jakedoes
 
-import dev.jakedoes.mumble.domain.Version
-import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.decodeFromByteArray
-import kotlinx.serialization.encodeToByteArray
-import kotlinx.serialization.protobuf.ProtoBuf
-
-@OptIn(ExperimentalSerializationApi::class)
 fun main() {
-    val version = Version(1, 2, "hello", "linux", "whatever")
-    val bytes = ProtoBuf.encodeToByteArray(version)
-    println(bytes.toAsciiHexString())
-    val obj = ProtoBuf.decodeFromByteArray<Version>(bytes)
-    println(obj)
-}
-
-fun ByteArray.toAsciiHexString() = joinToString("") {
-    if (it in 32..127) it.toInt().toChar().toString() else
-        "{${it.toUByte().toString(16).padStart(2, '0').uppercase()}}"
+    println("hi!")
 }
