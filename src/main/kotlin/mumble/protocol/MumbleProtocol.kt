611909503f3838b6c8c50c7e994891bf38b69eed
Jake Allan <jjemail1995@gmail.com>
2025-05-25 18:30:31 +0100

    feat(protocol): encode/decode according to Mumble protocol

--- src/main/kotlin/mumble/protocol/MumbleProtocol.kt
+++ src/main/kotlin/mumble/protocol/MumbleProtocol.kt
@@ -2,40 +2,34 @@
 
 package dev.jakedoes.mumble.protocol
 
-import dev.jakedoes.mumble.domain.Authenticate
-import dev.jakedoes.mumble.domain.Version
+import dev.jakedoes.mumble.domain.Message
+import dev.jakedoes.mumble.domain.Message.*
 import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromByteArray
 import kotlinx.serialization.encodeToByteArray
 import kotlinx.serialization.protobuf.ProtoBuf
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 
-enum class MessageInfo(id: ByteArray) {
-    Version(byteArrayOf(0x00, 0x00)),
-    Authenticate(byteArrayOf(0x00, 0x02)),
-    Ping(byteArrayOf(0x00, 0x03)),
-}
-
-fun encode(version: Version): ByteArray {
-    val id: ByteArray = ByteBuffer.allocate(2)
-        .order(ByteOrder.BIG_ENDIAN)
-        .putInt(0)
-        .array()
-
-    val bytes: ByteArray = ProtoBuf.encodeToByteArray(version)
+fun encode(message: Message): ByteArray {
+    val bytes: ByteArray = ProtoBuf.encodeToByteArray(message)
 
     val length: ByteArray = ByteBuffer.allocate(4)
         .order(ByteOrder.BIG_ENDIAN)
         .putInt(bytes.size)
         .array()
 
-    return id + length + bytes
+    return message.id() + length + bytes
 }
 
-fun encode(authenticate: Authenticate): ByteArray {
-    return TODO("Provide the return value")
+fun decode(bytes: ByteArray): Message {
+    return ProtoBuf.decodeFromByteArray<Message>(bytes.copyOfRange(6, bytes.size))
 }
 
-fun main() {
-    encode(Version(release="release"))
+private fun Message.id(): ByteArray {
+    return when (this) {
+        is Version -> byteArrayOf(0x00, 0x00)
+        is Authenticate -> byteArrayOf(0x00, 0x02)
+        is Ping -> byteArrayOf(0x00, 0x03)
+    }
 }
