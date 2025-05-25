63b80918f1c22a2b0a20cdf687f9e7adc33dfe66
Jake Allan <jjemail1995@gmail.com>
2025-05-25 17:42:30 +0100

    feat(proto): initial start on using kotlinx-serialization-protobuf (#3)
    Initial attempt at using `kotlinx-serialization-protobuf`, as outlined here:
    https://github.com/Kotlin/kotlinx.serialization/blob/master/docs/formats.md#protobuf-experimental

--- src/main/kotlin/Arithmetic.kt
+++
@@ -1,3 +0,0 @@
-package dev.jakedoes
-
-fun sum(a: Int, b: Int): Int = a + b
---
+++ src/test/kotlin/mumble/domain/ProtobufTest.kt
@@ -0,0 +1,20 @@
+package mumble.domain
+
+import dev.jakedoes.mumble.domain.Version
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.decodeFromByteArray
+import kotlinx.serialization.encodeToByteArray
+import kotlinx.serialization.protobuf.ProtoBuf
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ProtobufTest {
+    @OptIn(ExperimentalSerializationApi::class)
+    @Test
+    fun encodeDecodeTest() {
+        val version = Version(1, 2, "hello", "linux", "whatever")
+        val serialized = ProtoBuf.encodeToByteArray(version)
+        val deserialized = ProtoBuf.decodeFromByteArray<Version>(serialized)
+        assertEquals(version, deserialized)
+    }
+}
