04c12901b3ef35247e7a7fe30e2661375b675784
Jake Allan <jjemail1995@gmail.com>
2025-05-26 13:04:25 +0100

    feat(messages): add more message types (#17)

--- src/main/kotlin/mumble/protocol/MumbleProtocol.kt
+++ src/main/kotlin/mumble/protocol/MumbleProtocol.kt
@@ -3,7 +3,10 @@
 package dev.jakedoes.mumble.protocol
 
 import dev.jakedoes.mumble.domain.Authenticate
+import dev.jakedoes.mumble.domain.ChannelState
+import dev.jakedoes.mumble.domain.CryptSetup
 import dev.jakedoes.mumble.domain.Ping
+import dev.jakedoes.mumble.domain.TextMessage
 import dev.jakedoes.mumble.domain.Version
 import io.github.oshai.kotlinlogging.KotlinLogging
 import kotlinx.serialization.ExperimentalSerializationApi
@@ -13,6 +16,7 @@ import kotlinx.serialization.encodeToHexString
 import kotlinx.serialization.protobuf.ProtoBuf
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
+import kotlin.reflect.KClass
 
 private val logger = KotlinLogging.logger { }
 
@@ -22,20 +26,34 @@ private fun ByteArray.toAsciiHexString() = joinToString("") {
 }
 
 object MumbleProtocol {
-    fun encode(message: Version): ByteArray {
-        logger.info { ProtoBuf.encodeToHexString(message) }
-        return encode(MessageType.Version.id, ProtoBuf.encodeToByteArray(message))
-    }
-
-    fun encode(message: Authenticate): ByteArray {
-        logger.info { ProtoBuf.encodeToHexString(message) }
-        return encode(MessageType.Authenticate.id, ProtoBuf.encodeToByteArray(message))
-    }
-
-    fun encode(message: Ping): ByteArray {
-        logger.info { ProtoBuf.encodeToHexString(message) }
-        return encode(MessageType.Ping.id, ProtoBuf.encodeToByteArray(message))
-    }
+    fun <T : Any> encode(message: T): ByteArray =
+        when (message) {
+            is Version -> {
+                logger.info { "Encoded a [Version], yielding the following hex: ${ProtoBuf.encodeToHexString<Version>(message)}" }
+                encode(MessageType.Version.id, ProtoBuf.encodeToByteArray<Version>(message))
+            }
+            is Authenticate -> {
+                logger.info { "Encoded a [Authenticate], yielding the following hex: ${ProtoBuf.encodeToHexString<Authenticate>(message)}" }
+                encode(MessageType.Authenticate.id, ProtoBuf.encodeToByteArray<Authenticate>(message))
+            }
+            is Ping -> {
+                logger.info { "Encoded a [Ping], yielding the following hex: ${ProtoBuf.encodeToHexString<Ping>(message)}" }
+                encode(MessageType.Ping.id, ProtoBuf.encodeToByteArray<Ping>(message))
+            }
+            is ChannelState -> {
+                logger.info { "Encoded a [ChannelState], yielding the following hex: ${ProtoBuf.encodeToHexString<ChannelState>(message)}" }
+                encode(MessageType.ChannelState.id, ProtoBuf.encodeToByteArray<ChannelState>(message))
+            }
+            is CryptSetup -> {
+                logger.info { "Encoded a [CryptSetup], yielding the following hex: ${ProtoBuf.encodeToHexString<CryptSetup>(message)}" }
+                encode(MessageType.CryptSetup.id, ProtoBuf.encodeToByteArray<CryptSetup>(message))
+            }
+            is TextMessage -> {
+                logger.info { "Encoded a [TextMessage], yielding the following hex: ${ProtoBuf.encodeToHexString<TextMessage>(message)}" }
+                encode(MessageType.TextMessage.id, ProtoBuf.encodeToByteArray<TextMessage>(message))
+            }
+            else -> throw IllegalArgumentException("Unknown type for encoding: ${message.javaClass}")
+        }
 
     private fun encode(id: Short, payload: ByteArray): ByteArray {
         val encoding = ByteBuffer.allocate(2 + 4 + payload.size)
@@ -49,12 +67,18 @@ object MumbleProtocol {
         return encoding
     }
 
-    fun decodeVersion(bytes: ByteArray): Version =
-        ProtoBuf.decodeFromByteArray<Version>(bytes.copyOfRange(6, bytes.size))
+    inline fun <reified T : Any> decode(bytes: ByteArray, clazz: KClass<T>): T =
+        decodePayload(bytes.copyOfRange(6, bytes.size), clazz)
 
-    fun decodeAuthenticate(bytes: ByteArray): Authenticate =
-        ProtoBuf.decodeFromByteArray<Authenticate>(bytes.copyOfRange(6, bytes.size))
-
-    fun decodePing(bytes: ByteArray): Ping =
-        ProtoBuf.decodeFromByteArray<Ping>(bytes.copyOfRange(6, bytes.size))
+    inline fun <reified T : Any> decodePayload(payload: ByteArray, clazz: KClass<T>): T {
+        return when (clazz) {
+            Version::class -> ProtoBuf.decodeFromByteArray<Version>(payload) as T
+            Authenticate::class -> ProtoBuf.decodeFromByteArray<Authenticate>(payload) as T
+            Ping::class -> ProtoBuf.decodeFromByteArray<Ping>(payload) as T
+            ChannelState::class -> ProtoBuf.decodeFromByteArray<ChannelState>(payload) as T
+            CryptSetup::class -> ProtoBuf.decodeFromByteArray<CryptSetup>(payload) as T
+            TextMessage::class -> ProtoBuf.decodeFromByteArray<TextMessage>(payload) as T
+            else -> throw IllegalArgumentException("Unknown type for decoding: ${clazz.simpleName}")
+        }
+    }
 }
