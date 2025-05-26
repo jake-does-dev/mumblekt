46a7de604bcd45ea1ea61ea8027d806b026c077a
Jake Allan <jjemail1995@gmail.com>
2025-05-26 14:22:05 +0100

    feat(client): introduce mumble client (#19)
    Introduces a mumble client, alongside two Kotlin Coroutines:
    - one that regularly `Ping`s the server, as defined by
    https://github.com/mumble-voip/mumble/blob/master/docs/dev/network-protocol/establishing_connection.md#version-exchange
    - one that handles the messages

--- src/main/kotlin/mumble/protocol/MumbleProtocol.kt
+++ src/main/kotlin/mumble/protocol/MumbleProtocol.kt
@@ -6,6 +6,7 @@ import dev.jakedoes.mumble.domain.Authenticate
 import dev.jakedoes.mumble.domain.ChannelState
 import dev.jakedoes.mumble.domain.CryptSetup
 import dev.jakedoes.mumble.domain.Ping
+import dev.jakedoes.mumble.domain.ServerSync
 import dev.jakedoes.mumble.domain.TextMessage
 import dev.jakedoes.mumble.domain.Version
 import io.github.oshai.kotlinlogging.KotlinLogging
@@ -25,32 +26,45 @@ private fun ByteArray.toAsciiHexString() = joinToString("") {
         "{${it.toUByte().toString(16).padStart(2, '0').uppercase()}}"
 }
 
+/**
+ * The ideal way to do this would be to have a `sealed interface`, and each of the serializable data classes implement
+ * that interface. Gives us exhaustive branches, and safety, which is nice.
+ *
+ * However, if we do this, then the serialized messages out of the Protobuf encoder have the fully qualified class
+ * name as part of its message - i.e., `dev.jakedoes.mumble.protocol.TextMessage`. Whilst this is valid for the
+ * Protobuf implementation, it does not play ball with Mumble's protocol. Consequently, we have to take a bit of
+ * verbosity here.
+ */
 object MumbleProtocol {
     fun <T : Any> encode(message: T): ByteArray =
         when (message) {
             is Version -> {
-                logger.info { "Encoded a [Version], yielding the following hex: ${ProtoBuf.encodeToHexString<Version>(message)}" }
+                logger.debug { "Encoded a [Version], yielding the following hex: ${ProtoBuf.encodeToHexString<Version>(message)}" }
                 encode(MessageType.Version.id, ProtoBuf.encodeToByteArray<Version>(message))
             }
             is Authenticate -> {
-                logger.info { "Encoded a [Authenticate], yielding the following hex: ${ProtoBuf.encodeToHexString<Authenticate>(message)}" }
+                logger.debug { "Encoded a [Authenticate], yielding the following hex: ${ProtoBuf.encodeToHexString<Authenticate>(message)}" }
                 encode(MessageType.Authenticate.id, ProtoBuf.encodeToByteArray<Authenticate>(message))
             }
             is Ping -> {
-                logger.info { "Encoded a [Ping], yielding the following hex: ${ProtoBuf.encodeToHexString<Ping>(message)}" }
+                logger.debug { "Encoded a [Ping], yielding the following hex: ${ProtoBuf.encodeToHexString<Ping>(message)}" }
                 encode(MessageType.Ping.id, ProtoBuf.encodeToByteArray<Ping>(message))
             }
             is ChannelState -> {
-                logger.info { "Encoded a [ChannelState], yielding the following hex: ${ProtoBuf.encodeToHexString<ChannelState>(message)}" }
+                logger.debug { "Encoded a [ChannelState], yielding the following hex: ${ProtoBuf.encodeToHexString<ChannelState>(message)}" }
                 encode(MessageType.ChannelState.id, ProtoBuf.encodeToByteArray<ChannelState>(message))
             }
             is CryptSetup -> {
-                logger.info { "Encoded a [CryptSetup], yielding the following hex: ${ProtoBuf.encodeToHexString<CryptSetup>(message)}" }
+                logger.debug { "Encoded a [CryptSetup], yielding the following hex: ${ProtoBuf.encodeToHexString<CryptSetup>(message)}" }
                 encode(MessageType.CryptSetup.id, ProtoBuf.encodeToByteArray<CryptSetup>(message))
             }
             is TextMessage -> {
-                logger.info { "Encoded a [TextMessage], yielding the following hex: ${ProtoBuf.encodeToHexString<TextMessage>(message)}" }
+                logger.debug { "Encoded a [TextMessage], yielding the following hex: ${ProtoBuf.encodeToHexString<TextMessage>(message)}" }
                 encode(MessageType.TextMessage.id, ProtoBuf.encodeToByteArray<TextMessage>(message))
+            }
+            is ServerSync -> {
+                logger.debug { "Encoded a [ServerSync], yielding the following hex: ${ProtoBuf.encodeToHexString<ServerSync>(message)}" }
+                encode(MessageType.ServerSync.id, ProtoBuf.encodeToByteArray<ServerSync>(message))
             }
             else -> throw IllegalArgumentException("Unknown type for encoding: ${message.javaClass}")
         }
@@ -63,7 +77,7 @@ object MumbleProtocol {
             .put(payload)
             .array()
 
-        logger.info { "Sending: ${encoding.toAsciiHexString()}" }
+        logger.debug { "Sending: ${encoding.toAsciiHexString()}" }
         return encoding
     }
 
@@ -78,6 +92,7 @@ object MumbleProtocol {
             ChannelState::class -> ProtoBuf.decodeFromByteArray<ChannelState>(payload) as T
             CryptSetup::class -> ProtoBuf.decodeFromByteArray<CryptSetup>(payload) as T
             TextMessage::class -> ProtoBuf.decodeFromByteArray<TextMessage>(payload) as T
+            ServerSync::class -> ProtoBuf.decodeFromByteArray<ServerSync>(payload) as T
             else -> throw IllegalArgumentException("Unknown type for decoding: ${clazz.simpleName}")
         }
     }
