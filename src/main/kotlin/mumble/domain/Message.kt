7357ee8bc8cda988452edd5bd739614a06a17156
Jake Allan <jjemail1995@gmail.com>
2025-05-26 07:33:29 +0100

    refactor(proto): refactor sealed interface

--- src/main/kotlin/mumble/domain/Message.kt
+++ src/main/kotlin/mumble/domain/Message.kt
@@ -1,7 +1,33 @@
 package dev.jakedoes.mumble.domain
 
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.protobuf.ProtoNumber
 
 @Serializable
-sealed interface Message {
-}
+sealed interface Message
+
+@Serializable
+@OptIn(ExperimentalSerializationApi::class)
+data class Authenticate(
+    @ProtoNumber(1) val username: String?,
+    @ProtoNumber(2) val password: String?
+) : Message
+
+@Serializable
+@OptIn(ExperimentalSerializationApi::class)
+object Ping : Message
+
+@Serializable
+@OptIn(ExperimentalSerializationApi::class)
+data class Version(
+    @ProtoNumber(1) val versionV1: Int? = null,
+    @ProtoNumber(2) val versionV2: Int? = null,
+    @ProtoNumber(3) val release: String? = null,
+    @ProtoNumber(4) val os: String? = null,
+    @ProtoNumber(5) val osVersion: String? = null,
+) : Message
+
+data class Unrecognised(
+    val reason: String
+) : Message
--- src/main/kotlin/mumble/domain/Unrecognised.kt
+++
@@ -1,5 +0,0 @@
-package dev.jakedoes.mumble.domain
-
-data class Unrecognised(
-    val reason: String
-) : Message
